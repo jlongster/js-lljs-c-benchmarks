ff6a6297d5e3e5a223c074c72e5811729cf297a2
James Long <jlongster@jlongster.com>
2012-10-28 17:37:44 -0400

    initial file & makefile

---
+++ Makefile
@@ -0,0 +1,3 @@
+
+all:
+	./LLJS/bin/ljc main.ljs > main.js
---
+++ main.js
@@ -0,0 +1,5 @@
+(function (exports) {
+  var util = require('util');
+  var foo = 5;
+  util.puts(foo);
+}.call(this, typeof exports === 'undefined' ? main_ljs = {} : exports));
